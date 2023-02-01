b125ce59c574c3905966953e502d67ceb7608ab6
Guilherme Borges de Assis <gbdeassis@hotmail.com.br>
2023-02-01 14:02:46 -0300

    Adição do Curso Essencial JS aula 01 e 02
    Adição do Curso JS e aulas 01 e 02

---
+++ JS/Js Essencial/aula01.js
@@ -0,0 +1,43 @@
+// alert('Primeiro Codigo JS do Guilherme B de A');  
+
+// Tipos de formas de declaração de variavel
+// var idade_aluno = 30;
+// var IdadeAluno = 30;
+// var idadeAluno = 30;
+
+// alert(idadealuno);
+
+// var idade = 25;
+// var nome = 'Guilherme';
+// var casado = false;
+
+var idade = 25, nome = 'Guilherme', casado = false;
+var formacao = ['HTML', 'CSS', 'JS'];
+var pessoa = {idade: 30, nome: 'Guilherme', casado: false};
+var texto = ''; //diferente de null
+var texto = null; 
+var x = 'um texto aqui';
+var x = 'Aqui mudou o texto';
+
+var x = 10, y = 5, z = 2;
+// x *=5, x /= 5;
+// x+=5;
+//x--, x++;
+
+// console.log(idade, nome, 'casado ' + casado);
+// console.log(formacao)
+// console.log(formacao[1])
+// console.log(pessoa.nome)
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+console.log(x);
+
+/* var
+function 
+break
+return
+continue
+*/
+
