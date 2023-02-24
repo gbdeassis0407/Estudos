c51c2654fd9749761ea2e676450408effa2691a4
Guilherme Borges de Assis <gbdeassis@hotmail.com.br>
2023-02-24 14:00:46 -0300

    Atualização de aula e exercícios HTML

--- JS/fonte.js
+++ JS/fonte.js
@@ -1,1 +1,19 @@
-console.log("teste")
+'use strict'
+
+const switcher = document.querySelector('.btn')
+
+switcher.addEventListener('click', darkTheme())
+
+function darkTheme(){
+    document.body.classList.toggle('dark-theme');
+
+    var className = document.body.className;
+
+    if (className == 'light-theme'){
+        this.textContent = 'Escuridão';
+        console.log('minha classe e: ' + className);
+    } else {
+        this.textContent = 'Luz'
+    }
+
+}
--- Java/Aprendendo/src/Aula_07.java
+++ Java/Aprendendo/src/Aula_07.java
@@ -1,7 +1,7 @@
 public class Aula_07 {
     public static void main(String[] args) {
-        Conta conta1; //Cria a variavel do tipo Conta1 (que é uma classe)
-        conta1 = new Conta(); // o new cria o objeto
+        // Conta conta1; //Cria a variavel do tipo Conta1 (que é uma classe)
+        // conta1 = new Conta(); // o new cria o objeto
 
     }
 }
